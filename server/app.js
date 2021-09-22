2cb40be495814b352787988858047bc00c23914d
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-22 23:26:48 +0530

    implement middleware

--- server/app.js
+++ server/app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const middleware = (req, res, next)=> {
+    console.log("Hello Middleware")
+    next();
+}
 app.get('/', (req, res) => {
     res.send("Hello from the server");
 });
 app.get('/contact', (req, res) => {
     res.send("Hello from the Contact");
 });
-app.get('/about', (req, res) => {
+app.get('/about', middleware, (req, res) => {
+    console.log(`After middleware`);
     res.send("Hello from the About");
 });
 app.get('/signin', (req, res) => {
