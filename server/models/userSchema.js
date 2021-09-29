55f6a8ab353f57a42af017f194054aee72936dd1
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-29 23:37:03 +0530

    Create email field in userSchema

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required:true,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
