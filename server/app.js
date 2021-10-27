bc23f24e479dff0d4871ff81ffb463af6072139c
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-28 00:29:43 +0530

    Use postman to post data

--- server/app.js
+++ server/app.js
@@ -6,30 +6,34 @@ const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 require('./db/connection');
 
+app.use(express.json());
+
+app.use(require('./router/auth'));
+
 const User = require('./models/userSchema');
 
 const PORT = process.env.PORT;
 
-const middleware = (req, res, next) => {
-  console.log('Hello Middleware');
-  next();
-};
-app.get('/', (req, res) => {
-  res.send('Hello from the server');
-});
-app.get('/contact', (req, res) => {
-  res.send('Hello from the Contact');
-});
-app.get('/about', middleware, (req, res) => {
-  console.log(`After middleware`);
-  res.send('Hello from the About');
-});
-app.get('/signin', (req, res) => {
-  res.send('Hello from the Sign-In');
-});
-app.get('/signup', (req, res) => {
-  res.send('Hello from the Sign-Up');
-});
+// const middleware = (req, res, next) => {
+//   console.log('Hello Middleware');
+//   next();
+// };
+// app.get('/', (req, res) => {
+//   res.send('Hello from the server');
+// });
+// app.get('/contact', (req, res) => {
+//   res.send('Hello from the Contact');
+// });
+// app.get('/about', middleware, (req, res) => {
+//   console.log(`After middleware`);
+//   res.send('Hello from the About');
+// });
+// app.get('/signin', (req, res) => {
+//   res.send('Hello from the Sign-In');
+// });
+// app.get('/signup', (req, res) => {
+//   res.send('Hello from the Sign-Up');
+// });
 
 app.listen(PORT, (err) => {
   if (err) {
