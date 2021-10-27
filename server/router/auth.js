329473e183978ddb905bd3ee5b320407771697aa
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-28 00:28:37 +0530

    Created router folder

---
+++ server/router/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello Manoj singh Rana');
+});
+
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.json({ message: req.body });
+});
+
+module.exports = router;
