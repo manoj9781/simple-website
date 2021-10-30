619875af5f23fd841c1751176083e932c9f00158
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-30 22:15:30 +0530

    Install bcrypt js

--- server/app.js
+++ server/app.js
@@ -14,27 +14,6 @@ const User = require('./models/userSchema');
 
 const PORT = process.env.PORT;
 
-// const middleware = (req, res, next) => {
-//   console.log('Hello Middleware');
-//   next();
-// };
-// app.get('/', (req, res) => {
-//   res.send('Hello from the server');
-// });
-// app.get('/contact', (req, res) => {
-//   res.send('Hello from the Contact');
-// });
-// app.get('/about', middleware, (req, res) => {
-//   console.log(`After middleware`);
-//   res.send('Hello from the About');
-// });
-// app.get('/signin', (req, res) => {
-//   res.send('Hello from the Sign-In');
-// });
-// app.get('/signup', (req, res) => {
-//   res.send('Hello from the Sign-Up');
-// });
-
 app.listen(PORT, (err) => {
   if (err) {
     console.log('Error in running the server', err);
