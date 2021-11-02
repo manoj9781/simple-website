6f1af752c16ce1a409ad9778a78910d23dc2df75
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-03 00:31:31 +0530

    Update the schema to store jwt

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,6 +28,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      }
+    }
+  ]
 });
 
 //Hashing the Password
@@ -38,6 +48,17 @@ userSchema.pre('save', async function (next) {
   }
   next();
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const User = mongoose.model('USER', userSchema);
 module.exports = User;
