7df75d45ed17590de802832d4ff78c70581e92bc
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-07 17:15:16 +0530

    Create Home Component

---
+++ client/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1 className="pt-5">HEllo Home</h1>
+            <p>We are the Mern Developer</p>
+        </div>
+    )
+}
+
+export default Home;
