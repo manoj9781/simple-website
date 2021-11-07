2ece1b184c89766924890fb419e6761da6381347
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-07 17:18:36 +0530

    Render all the component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div>
-     <Navbar></Navbar>
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
