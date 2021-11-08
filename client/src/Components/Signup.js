855d409fb92667f95858d057ba36fdbef3e42041
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-08 23:22:39 +0530

    Create sign up fields

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
+import './signup.css';
+import signUp from '../images/signup.jpg';
+import { NavLink } from 'react-router-dom';
 
 function Signup() {
-    return (
-        <div>
-            <h1>Sign up</h1>
+  return (
+    <>
+      <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">Sign Up</h2>
+              <form className="register-form" id="register-form">
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <i class="zmdi zmdi-account materials-icon-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter your name"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    value="Register"
+                    name="signup"
+                    id="signup"
+                    className="form-submit"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="signup-image">
+              <figure>
+                <img src={signUp} alt="Pic" />
+              </figure>
+              <NavLink to="/login" className="signup-image-link">
+                I am already register
+              </NavLink>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
