a8b69872c8c65ee89840d645bed4d2479e0a4fb0
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-18 23:58:59 +0530

    Complete About Page UI and Tab list done

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -6,39 +6,28 @@ function About() {
   return (
     <>
       <div className="container emp-profile">
-        <form method="post">
+        <form method="">
           <div className="row">
-            <div className="col-md-4 ">
-              <img className="user-image" src={userImage} alt="UserPic" />
+            <div className="col-md-4">
+              <img className="user-image" src={userImage} alt="UserImage" />
             </div>
             <div className="col-md-6">
               <div className="profile-head">
-                <h5>Manoj Singh</h5>
+                <h5>Manoj Singh Rana</h5>
                 <h6>Web Developer</h6>
                 <p className="profile-rating mt-3 mb-5">
                   RANKINGS: <span>1/10</span>
                 </p>
-                <ul className="nav nav-tabs" role="tablist">
-                  <li className="nav-item">
-                    <a
-                      href="#home"
-                      className="nav-link active"
-                      id="home-tab"
-                      role="tab"
-                      data-toggle="tab"
-                    >
-                      About
+
+                <ul class="nav nav-tabs" role="tablist">
+                  <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#home">
+                     About
                     </a>
                   </li>
-                  <li className="nav-item">
-                    <a
-                      href="#profile"
-                      className="nav-link active"
-                      id="profile-tab"
-                      role="tab"
-                      data-toggle="tab"
-                    >
-                      TimeLine
+                  <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#profile">
+                      Timeline
                     </a>
                   </li>
                 </ul>
@@ -56,23 +45,39 @@ function About() {
           <div className="row">
             <div className="col-md-4">
               <div className="profile-work">
-                <p>WorK Link</p>
-                <a href="#">Youtube</a><br />
-                <a href="#">Facebook</a><br />
-                <a href="#">Github</a><br />
-                <a href="#">Linkden</a><br />
-                <a href="#">Instagram</a><br />
+                <p>Work Links</p>
+                <a href="https://www.codechef.com/users/manoj_singh">
+                  CodeChef
+                </a>
+                <br />
+                <a href="https://www.instagram.com/r_a_n_a_03/">
+                  Instagram
+                </a>{' '}
+                <br />
+                <a href="https://www.instagram.com/r_a_n_a_03/">Youtube</a>{' '}
+                <br />
+                <a href="https://www.instagram.com/r_a_n_a_03/">
+                  Facebook
+                </a>{' '}
+                <br />
+                <a href="https://www.instagram.com/r_a_n_a_03/">Linkden</a>{' '}
+                <br />
               </div>
             </div>
             <div className="col-md-8 pl-5 about-info">
-              <div className="tab-content profile-tab" id="myContentTab">
-                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                  <div className="row mt-3">
+              <div className="tab-content profile-tab" id="myTabContent">
+                <div
+                  className="tab-pane show active fade"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                >
+                  <div className="row">
                     <div className="col-md-6">
-                      <label>USER ID</label>
+                      <label>User Id</label>
                     </div>
                     <div className="col-md-6">
-                      <p>87087060878</p>
+                      <p>57632819</p>
                     </div>
                   </div>
                   <div className="row mt-3">
@@ -85,10 +90,10 @@ function About() {
                   </div>
                   <div className="row mt-3">
                     <div className="col-md-6">
-                      <label>Profession</label>
+                      <label>Specialization</label>
                     </div>
                     <div className="col-md-6">
-                      <p>Developer</p>
+                      <p>Web Development</p>
                     </div>
                   </div>
                   <div className="row mt-3">
@@ -96,12 +101,41 @@ function About() {
                       <label>University</label>
                     </div>
                     <div className="col-md-6">
-                      <p>LPU</p>
+                      <p>Lovely Professional University</p>
                     </div>
                   </div>
                 </div>
-                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <div
+                  className="tab-pane fade"
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
                   <div className="row">
+                    <div className="col-md-6">
+                      <label>Experience</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Beginer</p>
+                    </div>
+                  </div>
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Experience</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Beginer</p>
+                    </div>
+                  </div>
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Experience</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Beginer</p>
+                    </div>
+                  </div>
+                  <div className="row mt-3">
                     <div className="col-md-6">
                       <label>Experience</label>
                     </div>
