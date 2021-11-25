89d42e1086d533c54b558c043765521bbe3b3054
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-25 23:38:08 +0530

    Send data from front-end to back-end succesfully

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 dotenv.config({ path: './config.env' });
 require('./db/connection');
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(require('./router/auth'));
 
 const User = require('./models/userSchema');
-
+app.use(cors());
 const PORT = process.env.PORT;
 
 app.listen(PORT, (err) => {
