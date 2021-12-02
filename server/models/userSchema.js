8c81dcde2fff4b141c9caad2901b69ced7624e12
Manoj Singh <manojsinghrana467@gmail.com>
2021-12-02 12:11:20 +0530

    Update userSchema for storing the messages

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -27,6 +27,30 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  messages: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: Number,
+        required: true,
+      },
+      message: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   tokens: [
     {
       token: {
@@ -56,6 +80,16 @@ userSchema.methods.generateAuthToken = async function () {
     return token;
   } catch (error) {
     console.log(error);
+  }
+};
+
+userSchema.methods.addMessage = async function (name, email, phone, message) {
+  try {
+    this.messages = this.messages.concat({ name, email, phone, message });
+    await this.save();
+    return this.messages;
+  } catch (error) {
+    console.log('Error', error);
   }
 };
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -94,7 +94,7 @@ router.post('/signin', async (req, res) => {
       const token = await userLogin.generateAuthToken();
       console.log(token);
 
-       res.cookie('jwtToken', token, {
+      res.cookie('jwtToken', token, {
         expires: new Date(Date.now() + 25892000000),
         httpOnly: true,
       });
@@ -103,7 +103,6 @@ router.post('/signin', async (req, res) => {
       } else {
         return res.status(200).json({ message: 'User sign in succesfully' });
       }
-     
     } else {
       return res.status(400).json({ error: 'Invalid credientials' });
     }
@@ -116,28 +115,37 @@ router.get('/about', authenticate, (req, res) => {
   res.send(req.rootUser);
 });
 
-router.get("/getdata", authenticate, (req, res) => {
+router.get('/getdata', authenticate, (req, res) => {
   res.send(req.rootUser);
-})
+});
 
-router.get('/contact', authenticate, async (req, res) => {
+router.post('/contact', authenticate, async (req, res) => {
   try {
+    console.log(req.body);
     const { name, email, phone, message } = req.body;
 
     if (!name || !email || !phone || !message) {
-      console.log("Error in constact form");
-      return res.json({ error: "Please fill the require field" });
+      console.log('Error in contact form');
+      return res.json({ message: 'Please fill the require field' });
     }
 
     const userContact = await User.findOne({ _id: req.userID });
 
     if (userContact) {
-      const userMessage = await userContact.addMessage(name, email, phone, message);
-    }
+      const userMessage = await userContact.addMessage(
+        name,
+        email,
+        phone,
+        message
+      );
 
+      await userContact.save();
+
+      res.status(201).json({ message: 'Message sent succesfully' });
+    }
   } catch (error) {
-    console.log("Error", error);
+    console.log('Error', error);
   }
-})
+});
 
 module.exports = router;
