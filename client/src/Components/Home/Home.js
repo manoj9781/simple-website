105a6f6a67e67281c3b2ae01b885f04a0ed15859
Manoj Singh <manojsinghrana467@gmail.com>
2021-12-02 12:38:34 +0530

    Show dynamic data on home page done

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
 function Home() {
-    return (
-        <>
-            
-            <div className="home-page">
-                <div className="home-div">
-                    <p> Welcome </p>
-                    <h2>We Are the Mern Developer </h2>
-                </div>
-            </div>
-        </>
-    )
+    const [userName, setUserName] = useState('');
+    const [show, setShow] = useState(false);
+  const homePage = async () => {
+    try {
+      const res = await fetch('/getdata', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await res.json();
+        setUserName(data.name);
+        setShow(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    homePage();
+  }, []);
+
+  return (
+    <>
+      <div className="home-page">
+        <div className="home-div">
+          <p> Welcome </p>
+          <h2> {userName} </h2>
+          <h2> {show ? "Happy, to see you Back" : "We Are the Mern Developer "} </h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
